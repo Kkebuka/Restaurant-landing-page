898bfa4d8315244ba6f2e49d28b09a2350e97cdc
kkebuka <kkebuka@gmail.com>
2024-06-20 20:04:51 +0100

    minor update to App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
+import Testimonials from "./components/Testimonials";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Work />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
